5e9cbf18193b3e5e9e3b43e15bbf8c07ca878682
Enrique A <emoralesdelosrios@icloud.com>
2025-11-30 18:46:53 +0000

    le agregue un handleClick para ver como funcionan los eventos

--- src/js/components/Lights.jsx
+++ src/js/components/Lights.jsx
@@ -4,13 +4,17 @@ const Lights = () => {
 
     const [newColor, setNewColor] = useState("red");
 
+    const handleClick = (color) => {
+        console.log("cambia a", color)
+        setNewColor(color)
+    }
     return (
         <div className="k">
             <div className="tube"></div>
             <div className="traffic-light">
-                <div onClick={() => setNewColor("red")} className={"light bg-danger" + (newColor === "red" ? " shine" : "")}></div>
-                <div onClick={() => setNewColor("yellow")} className={"light bg-warning" + (newColor === "yellow" ? " shine" : "")}></div>
-                <div onClick={() => setNewColor("green")} className={"light bg-success" + (newColor === "green" ? " shine" : "")}></div>
+                <div onClick={() => handleClick("red")} className={"light bg-danger" + (newColor === "red" ? " shine" : "")}></div>
+                <div onClick={() => handleClick("yellow")} className={"light bg-warning" + (newColor === "yellow" ? " shine" : "")}></div>
+                <div onClick={() => handleClick("green")} className={"light bg-success" + (newColor === "green" ? " shine" : "")}></div>
             </div>
         </div>
     );
